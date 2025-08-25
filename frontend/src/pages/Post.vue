59490512ecb36896ec07d5a7d175f42f12ce6909
Qu1et-x <lum27149@gmail.com>
2025-08-25 12:17:07 +0800

    fixed

---
+++ frontend/src/pages/Post.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="page-post">
+    <h1>Post</h1>
+  </div>
+</template>
+
+<script>
+export default { name: "Post" };
+</script>
--- frontend/src/pages/Posts.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="page-posts">
-    <h1>Posts</h1>
-  </div>
-</template>
-
-<script>
-export default { name: 'Posts' }
-</script>
